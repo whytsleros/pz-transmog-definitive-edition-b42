3ea7f48458e78bde72b823d0e0d455bcba59f3b0
mxswat <massimocacciapaglia.13@gmail.com>
2023-04-29 15:49:17 +0100

    1k tmog items

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var path = require('path');
 var uuid_1 = require("uuid");
 var fs_1 = require("fs");
-var path = require('path');
-var generateClothingItemXml = function (guid) {
-    return "\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<clothingItem>\n  <m_MaleModel></m_MaleModel>\n  <m_FemaleModel></m_FemaleModel>\n  <m_GUID>".concat(guid, "</m_GUID>\n  <m_Static>false</m_Static>\n  <m_AllowRandomHue>false</m_AllowRandomHue>\n  <m_AllowRandomTint>false</m_AllowRandomTint>\n  <m_AttachBone></m_AttachBone>\n  <m_BaseTextures>emptytexture</m_BaseTextures>\n</clothingItem>\n");
-};
-var itemIds = new Array(1000).fill(1).map(function (u) { return (0, uuid_1.v4)(); });
-var fileGuidTableFiles = [];
-for (var i = 0; i < itemIds.length; i++) {
-    var guid = itemIds[i];
-    var clothingItemXml = generateClothingItemXml(guid);
-    var clothingItemXmlPath = path.join(__dirname, "./Contents/mods/TransmogRebuild/media/clothing/clothingItems/TransmogItem_".concat(i, ".xml"));
-    (0, fs_1.writeFileSync)(clothingItemXmlPath, clothingItemXml, 'utf8');
-    fileGuidTableFiles.push("\n  <path>media/clothing/clothingItems/TransmogItem_".concat(i, ".xml</path>\n  <guid>").concat(guid, "</guid>"));
-    console.log('Generated Item #', i);
+var tmogItems = new Array(1000).fill(1).map(function (u, i) {
+    var guid = (0, uuid_1.v4)();
+    return {
+        guid: guid,
+        clothingItemPath: "media/clothing/clothingItems/TransmogItem_".concat(i, ".xml"),
+        clothingItemXml: "\n    <?xml version=\"1.0\" encoding=\"utf-8\"?>\n      <clothingItem>\n        <m_MaleModel></m_MaleModel>\n        <m_FemaleModel></m_FemaleModel>\n        <m_GUID>".concat(guid, "</m_GUID>\n        <m_Static>false</m_Static>\n        <m_AllowRandomHue>false</m_AllowRandomHue>\n        <m_AllowRandomTint>false</m_AllowRandomTint>\n        <m_AttachBone></m_AttachBone>\n        <m_BaseTextures>emptytexture</m_BaseTextures>\n      </clothingItem>\n    ").trim(),
+        scriptItemName: "TransmogItem_".concat(i),
+        scriptItem: "\n    item TransmogItem_".concat(i, "\n    {\n      DisplayCategory = Accessory,\n      Weight\t=\t0,\n      Type\t=\tClothing,\n      DisplayName\t=\tTransmogItem_").concat(i, ",\n      Icon\t=\tBelt,\n      BodyLocation = Belt,\n      ClothingItem = TransmogItem_").concat(i, ",\n      WorldStaticModel = TShirt_Ground,\n    }").trim()
+    };
+});
+for (var i = 0; i < tmogItems.length; i++) {
+    var tmogItem = tmogItems[i];
+    (0, fs_1.writeFileSync)(path.join(__dirname, "./Contents/mods/TransmogRebuild", tmogItem.clothingItemPath), tmogItem.clothingItemXml, 'utf8');
 }
-var fileGuidTableHead = "\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<fileGuidTable>\n\t<files>\n";
-var fileGuidTableFoot = "\n  </files>\n</fileGuidTable>\n";
-var fileGuidTable = (fileGuidTableHead + fileGuidTableFiles.join('\n') + fileGuidTableFoot).trim();
-var fileGuidTableXmlPath = path.join(__dirname, "Contents/mods/TransmogRebuild/media/fileGuidTable.xml");
-(0, fs_1.writeFileSync)(fileGuidTableXmlPath, fileGuidTable, 'utf8');
+var fileGuidTable = "\n<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<fileGuidTable>\n  <files>\n  ".concat(tmogItems.map(function (_a, i) {
+    var guid = _a.guid;
+    return ("\n  <path>media/clothing/clothingItems/TransmogItem_".concat(i, ".xml</path>\n  <guid>").concat(guid, "</guid>"));
+}).join('\n'), "\n  </files>\n</fileGuidTable>\n").trim();
+(0, fs_1.writeFileSync)(path.join(__dirname, "Contents/mods/TransmogRebuild/media/fileGuidTable.xml"), fileGuidTable, 'utf8');
+var scriptItem = "\nmodule TransmogRebuild {\n\timports { Base }\n\n  ".concat(tmogItems.map(function (_a) {
+    var scriptItem = _a.scriptItem;
+    return scriptItem;
+}).join('\n\n\t'), "\n\n}").trim();
+(0, fs_1.writeFileSync)(path.join(__dirname, "Contents/mods/TransmogRebuild/media/scripts/TransmogItems.txt"), scriptItem, 'utf8');
--- index.ts
+++ index.ts
@@ -1,54 +1,67 @@
-import { v4 as uuidv4 } from 'uuid';
-
-import { writeFileSync } from 'fs';
 const path = require('path');
+import { v4 as uuidv4 } from 'uuid';
+import { writeFileSync } from 'fs';
 
-const generateClothingItemXml = (guid) =>
-  `
-<?xml version="1.0" encoding="utf-8"?>
-<clothingItem>
-  <m_MaleModel></m_MaleModel>
-  <m_FemaleModel></m_FemaleModel>
-  <m_GUID>${guid}</m_GUID>
-  <m_Static>false</m_Static>
-  <m_AllowRandomHue>false</m_AllowRandomHue>
-  <m_AllowRandomTint>false</m_AllowRandomTint>
-  <m_AttachBone></m_AttachBone>
-  <m_BaseTextures>emptytexture</m_BaseTextures>
-</clothingItem>
-`
+const tmogItems = new Array(1000).fill(1).map((u, i) => {
+  const guid = uuidv4()
+  return {
+    guid,
+    clothingItemPath: `media/clothing/clothingItems/TransmogItem_${i}.xml`,
+    clothingItemXml: `
+    <?xml version="1.0" encoding="utf-8"?>
+      <clothingItem>
+        <m_MaleModel></m_MaleModel>
+        <m_FemaleModel></m_FemaleModel>
+        <m_GUID>${guid}</m_GUID>
+        <m_Static>false</m_Static>
+        <m_AllowRandomHue>false</m_AllowRandomHue>
+        <m_AllowRandomTint>false</m_AllowRandomTint>
+        <m_AttachBone></m_AttachBone>
+        <m_BaseTextures>emptytexture</m_BaseTextures>
+      </clothingItem>
+    `.trim(),
+    scriptItemName: `TransmogItem_${i}`,
+    scriptItem: `
+    item TransmogItem_${i}
+    {
+      DisplayCategory = Accessory,
+      Weight	=	0,
+      Type	=	Clothing,
+      DisplayName	=	TransmogItem_${i},
+      Icon	=	Belt,
+      BodyLocation = Belt,
+      ClothingItem = TransmogItem_${i},
+      WorldStaticModel = TShirt_Ground,
+    }`.trim()
+  } as const;
+});
 
-const itemIds = new Array(1000).fill(1).map(u => uuidv4());
-const fileGuidTableFiles: string[] = [];
+for (let i = 0; i < tmogItems.length; i++) {
+  const tmogItem = tmogItems[i];
 
-for (let i = 0; i < itemIds.length; i++) {
-  
-  const guid = itemIds[i];
-  const clothingItemXml = generateClothingItemXml(guid);
-  const clothingItemXmlPath = path.join(__dirname, `./Contents/mods/TransmogRebuild/media/clothing/clothingItems/TransmogItem_${i}.xml`);
-
-  writeFileSync(clothingItemXmlPath, clothingItemXml, 'utf8');
-
-  fileGuidTableFiles.push(`
-  <path>media/clothing/clothingItems/TransmogItem_${i}.xml</path>
-  <guid>${guid}</guid>`)
-
-  console.log('Generated Item #', i);
+  writeFileSync(path.join(__dirname, `./Contents/mods/TransmogRebuild`, tmogItem.clothingItemPath), tmogItem.clothingItemXml, 'utf8');
 }
 
-const fileGuidTableHead = `
+const fileGuidTable = `
 <?xml version="1.0" encoding="utf-8"?>
 <fileGuidTable>
-	<files>
-`;
-
-const fileGuidTableFoot = `
+  <files>
+  ${tmogItems.map(({ guid }, i) => (`
+  <path>media/clothing/clothingItems/TransmogItem_${i}.xml</path>
+  <guid>${guid}</guid>`)).join('\n')}
   </files>
 </fileGuidTable>
-`;
+`.trim();
 
-const fileGuidTable = (fileGuidTableHead + fileGuidTableFiles.join('\n') + fileGuidTableFoot).trim()
 
-const fileGuidTableXmlPath = path.join(__dirname, `Contents/mods/TransmogRebuild/media/fileGuidTable.xml`);
+writeFileSync(path.join(__dirname, `Contents/mods/TransmogRebuild/media/fileGuidTable.xml`), fileGuidTable, 'utf8');
 
-writeFileSync(fileGuidTableXmlPath, fileGuidTable, 'utf8');
+const scriptItem = `
+module TransmogRebuild {
+	imports { Base }
+
+  ${tmogItems.map(({scriptItem}) => scriptItem).join('\n\n\t')}
+
+}`.trim()
+
+writeFileSync(path.join(__dirname, `Contents/mods/TransmogRebuild/media/scripts/TransmogItems.txt`), scriptItem, 'utf8');
